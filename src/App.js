8ddf2c467b6ba9a460645c4c759be21149fc8e2c
kevin garcia <kevenmgs0610@gmail.com>
2022-09-26 08:57:05 -0500

    prueba2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [linea1, setlinea1] = useState ('');
+  const [linea2, setlinea2] = useState ("");
+  const [imagen, setimagen] = useState ('');
+
+  function transcribirLinea1(e){
+    setlinea1(e.target.value);
+  }
+
+  function transcribirLinea2(e){
+    setlinea2(e.target.value);
+  }
+
+  const pasarImagen = function (e){
+    setimagen(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={pasarImagen}>
+        <option value="CIVIL">Ing.civil</option>
+        <option value="BIOLOGIA">Biologia</option>
+        <option value="ADMINISTRACION">Lic.Administracion</option>
+      </select><br/>
+
+      <input onChange={transcribirLinea1} type="text" placeholder="Texto 1"></input><br/>
+      <input onChange={transcribirLinea2} type="text" placeholder="Texto 2"></input><br/>
+      <button>Examinar</button>
+
+      <div className='imagen'> 
+        <b className='p1'>{linea1}</b><br/>
+        <b className='p2'>{linea2}</b><br/>
+        <img src={'/img/' + imagen +'.jpg'}/>
+      </div>
     </div>
   );
 }
